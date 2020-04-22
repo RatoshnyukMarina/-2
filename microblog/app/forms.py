d8d20f022f43a8d0f44b9110a0f662bd371a250d
RatoshnyukMarina <63391991+RatoshnyukMarina@users.noreply.github.com>
2020-04-22 09:27:54 +0300

    Update forms.py
    +редактирование профиля

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import Required, Length
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +11,6 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+class EditForm(FlaskForm):
+    username = TextField('username', validators = [Required()])
+    about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
