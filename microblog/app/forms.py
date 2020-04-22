879606ddf339193dd791db3ce823fd45afe04867
RatoshnyukMarina <63391991+RatoshnyukMarina@users.noreply.github.com>
2020-04-22 16:00:13 +0300

    Update forms.py
     Проверка на уникальность ника и в случае неудачи предложение выбрать другой ник.

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms import TextField, BooleanField, TextAreaField
 from wtforms.validators import Required, Length
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -14,3 +15,20 @@ class LoginForm(FlaskForm):
 class EditForm(FlaskForm):
     username = TextField('username', validators = [Required()])
     about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
+
+
+
+    def __init__(self, original_username, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.original_username = original_username
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.username.data == self.original_username:
+            return True
+        user = User.query.filter_by(username = self.username.data).first()
+        if user != None:
+            self.username.errors.append('This nickname is already in use. Please choose another one.')
+            return False
+        return True
