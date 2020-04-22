326ccbb76e68e5999a01aca8ec2b774bae5a1148
RatoshnyukMarina <63391991+RatoshnyukMarina@users.noreply.github.com>
2020-04-22 09:39:24 +0300

    Update views.py
    функция обработки. редактирование профиля

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,3 +1,5 @@
+from forms import LoginForm, EditForm
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -7,3 +9,20 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit', methods = ['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.username.data
+        g.user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit.html',
+        form = form)
